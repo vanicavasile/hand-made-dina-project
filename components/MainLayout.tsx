e270410bd83bc709ef496a94d7855aaeb49491e8
Vasile <vvasile2797@gmail.com>
2023-03-26 01:17:08 +0200

    adjusted svg

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Head from "next/head";
 import Logo from "../assets/logo.png";
-import Favorite from "../assets/favorite.svg"
-import Cart from "../assets/cart.svg"
-import Image from "next/image"
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 export function MainLayout({ children, title = "HandMade | Dina" }) {
   return (
@@ -19,22 +20,28 @@ export function MainLayout({ children, title = "HandMade | Dina" }) {
 
       <nav>
         <div className="nav-link">
-        <Link href="/" className="logo">
-            <Image
-              priority
-              src={Logo} 
-              alt="Logo"
-            />
+          <Link href="/" className="logo">
+            <Image priority src={Logo} alt="Logo" />
           </Link>
           <div className="container-brooches">
             <Link href="/brooches" className="my-link nav-brooches">
-             <h3>Brooches</h3>
+              <h3>Brooches</h3>
             </Link>
             <div className="container-brooches-list">
               <ul className="ul-brooches-list">
-                <li className="brooches-list" > <Link href="/brooches/brooches_8_march">Broșe 8 Martie</Link></li>
-                <li className="brooches-list"> <Link href={"/brooches/brooches_martisor"}>Broșe Mărțișor</Link></li>
-                <li className="brooches-list"><Link href={"/brooches/brooches_new_year"}>Broșe Anul Nou</Link></li>
+                <li className="brooches-list">
+                  <Link href="/brooches/brooches_8_march">Broșe 8 Martie</Link>
+                </li>
+                <li className="brooches-list">
+                  <Link href={"/brooches/brooches_martisor"}>
+                    Broșe Mărțișor
+                  </Link>
+                </li>
+                <li className="brooches-list">
+                  <Link href={"/brooches/brooches_new_year"}>
+                    Broșe Anul Nou
+                  </Link>
+                </li>
               </ul>
             </div>
           </div>
@@ -52,24 +59,12 @@ export function MainLayout({ children, title = "HandMade | Dina" }) {
           <ul className="nav-ul-icons">
             <li className="nav-list-icons">
               <span>
-                <Image
-                  priority
-                  src={Favorite}
-                  height={20}
-                  width={20}
-                  alt="Favorite"
-                  />
+                <FontAwesomeIcon icon={faHeart} height="20px" />
               </span>
             </li>
             <li className="nav-list-icons">
               <span>
-                <Image
-                priority
-                src={Cart}
-                height={20}
-                width={20}
-                alt="Cart"
-                />
+                <FontAwesomeIcon icon={faCartShopping} height="20px" />
               </span>
             </li>
           </ul>
